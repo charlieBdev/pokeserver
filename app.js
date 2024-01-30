d7e25ad6e20ddc75ed3e2bee6e57a61c55f74d00
bigspambot <67501435+bigspambot@users.noreply.github.com>
2024-01-30 16:24:05 +0000

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const scoresRouter = require('./routes/scores-router');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
