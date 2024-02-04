38300196dedfb0cb3f245e7c049166c2c0f7c429
charlieBdev <chazbish@hotmail.com>
2024-02-04 20:53:43 +0000

    add cronjob

--- listen.js
+++ listen.js
@@ -1,7 +1,12 @@
 const app = require('./app');
+const cronJob = require('./pinger/pinger');
 
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}...`);
 });
+
+if (process.env.NODE_ENV === 'production') {
+	cronJob().start();
+}
---
+++ pinger/pinger.js
@@ -0,0 +1,13 @@
+const { Http2ServerRequest } = require('http2');
+const http = require('https');
+const cron = require('node-cron');
+
+const cronJob = () => {
+	return cron.schedule('59 14 * * * *', () => {
+		http.get('https://pokeserver-nt3v.onrender.com', () => {
+			console.log('Pinged!');
+		});
+	});
+};
+
+module.exports = cronJob;
