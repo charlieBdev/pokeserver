3ad825f9856a410de7040b13704141854594e804
Charlie B <67501435+charlieBdev@users.noreply.github.com>
2024-02-06 21:58:19 +0000

    Update pinger.js

--- pinger/pinger.js
+++ pinger/pinger.js
@@ -2,7 +2,7 @@ const http = require('https');
 const cron = require('node-cron');
 
 const cronJob = () => {
-	return cron.schedule('59 */14 * * * *', () => {
+	return cron.schedule('14 * * * *', () => {
 		http.get('https://pokeserver-nt3v.onrender.com', () => {
 			console.log('Pinged!');
 		});
